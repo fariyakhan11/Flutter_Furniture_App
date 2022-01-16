bbe53e0190645a3dd78b61bcd9af9a70be69a745
fasih-ansari <fasihansari39@gmail.com>
2022-01-17 01:12:41 +0500

    UI improvements

--- lib/screens/feeds.dart
+++ lib/screens/feeds.dart
@@ -1,32 +1,77 @@
+import 'package:badges/badges.dart';
+import 'package:ecommerce_app/consts/colors.dart';
+import 'package:ecommerce_app/consts/my_icons.dart';
 import 'package:ecommerce_app/models/product.dart';
+import 'package:ecommerce_app/provider/cart_provider.dart';
+import 'package:ecommerce_app/provider/favs_provider.dart';
 import 'package:ecommerce_app/provider/products.dart';
+import 'package:ecommerce_app/screens/wishlist.dart';
 import 'package:ecommerce_app/widgets/feeds_products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'cart.dart';
+
 
 class Feeds extends StatelessWidget {
   static const routeName = '/Feeds';
   @override
   Widget build(BuildContext context) {
-    final popular = ModalRoute.of(context)!.settings.arguments as String;
+    final popular = ModalRoute.of(context)!.settings.arguments;
     final productsProvider = Provider.of<Products>(context);
     List<Product> productsList = productsProvider.products;
     if (popular == 'popular') {
       productsList = productsProvider.popularProducts;
     }
     return Scaffold(
-        // body: FeedProducts(),
-        //     StaggeredGridView.countBuilder(
-        //     crossAxisCount: 4,
-        //     itemCount: 8,
-        //     itemBuilder: (BuildContext context, int index) => FeedProducts(
-        //     ), // Center // Container
-        // staggeredTileBuilder: (int index) =>
-        // new StaggeredTile.count(2, index.isEven ? 2 : 1),
-        // mainAxisSpacing: 4.0,
-        // crossAxisSpacing: 4.0,
-        //     )
-
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).cardColor,
+        title: Text('Feeds'),
+        actions: [
+          Consumer<FavsProvider>(
+                    builder: (_, favs, ch) => Badge(
+                      badgeColor: ColorConsts.cartBadgeColor,
+                      badgeContent: Text(
+                        favs.getFavsItems.length.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      animationType: BadgeAnimationType.slide,
+                      toAnimate: true,
+                      position: BadgePosition.topEnd(top: 5, end: 7),
+                      child: IconButton(
+                        icon: Icon(
+                          MyAppIcons.wishlist,
+                          color: ColorConsts.favColor,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushNamed(WishlistScreen.routeName);
+                        },
+                      ),
+                    ),
+                  ),
+                  Consumer<CartProvider>(
+                    builder: (_, cart, ch) => Badge(
+                      badgeColor: ColorConsts.cartBadgeColor,
+                      badgeContent: Text(
+                        cart.getCartItems.length.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      animationType: BadgeAnimationType.slide,
+                      toAnimate: true,
+                      position: BadgePosition.topEnd(top: 5, end: 7),
+                      child: IconButton(
+                        icon: Icon(
+                          MyAppIcons.cart,
+                          color: ColorConsts.cartColor,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pushNamed(CartScreen.routeName);
+                        },
+                      ),
+                    ),
+                  ),
+        ],),
         body: GridView.count(
       crossAxisCount: 2,
       childAspectRatio: 240 / 420,
